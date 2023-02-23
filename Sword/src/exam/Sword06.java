15b1f42eeb55726158353e3584cb7361c8879960
wang <894672511@qq.com>
2023-02-24 00:25:03 +0800

    剑指offer第二天06

---
+++ Sword/src/exam/Sword06.java
@@ -0,0 +1,53 @@
+package exam;//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+//
+//
+//
+// 示例 1：
+//
+// 输入：head = [1,3,2]
+//输出：[2,3,1]
+//
+//
+//
+// 限制：
+//
+// 0 <= 链表长度 <= 10000
+//
+// Related Topics 栈 递归 链表 双指针 👍 380 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Sword06 {
+    public int[] reversePrint(ListNode head) {
+        ListNode node = head;
+        int count = 0;
+        while (node != null) {
+            ++count;
+            node = node.next;
+        }
+        int[] nums = new int[count];
+        node = head;
+        for (int i = count - 1; i >= 0; --i) {
+            nums[i] = node.val;
+            node = node.next;
+        }
+        return nums;
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
